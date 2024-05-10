d65c834bdd7cb22189cf8ecc18dd07830eeb0e5b
jeferson <jefersonb88@gmail.com>
2024-05-10 08:31:51 -0300

    adição de id automatico na criação de registro

--- src/app.js
+++ src/app.js
@@ -6,64 +6,83 @@ app.use(cors());
 app.use(express.json());
 
 const mensagens = [
-  {
-    id:1,
-    nome:"Antonio",
-    mensagem: "Próximo ensaio 15/05"
-  },
-  {
-    id:2,
-    nome:"Antonio",
-    mensagem: "Teinar a coreografia"
-  }
-]
+	{
+		id: 1,
+		nome: "Antonio",
+		mensagem: "Próximo ensaio 15/05",
+	},
+	{
+		id: 2,
+		nome: "Antonio",
+		mensagem: "Teinar a coreografia",
+	},
+];
 
 function buscaMensagem(id) {
-  return mensagens.findIndex(msg => {
-    return msg.id === Number(id);
-  })
+	return mensagens.findIndex((msg) => {
+		return msg.id === Number(id);
+	});
 }
 
 const corsOptions = {
-  origin: ['https://consumo-api-3.netlify.app', 'http://localhost:5500'],
-  optionSucessStatus: 200
-}
+	origin: ["https://consumo-api-3.netlify.app", "http://localhost:5500"],
+	optionSucessStatus: 200,
+};
 
 app.get("/", cors(corsOptions), (req, res) => {
-  res.send("Rota inicial ok!");
+	res.send("Rota inicial ok!");
 });
 
 app.get("/hello/:name", cors(corsOptions), (req, res) => {
-  const name = req.params.name;
-  res.send(`Olá, ${name}!`);
+	const name = req.params.name;
+	res.send(`Olá, ${name}!`);
 });
 
 app.get("/mensagens", cors(corsOptions), (req, res) => {
-  // res.status(200).json("Mensagem:"+mensagens[1].mensagem);
-  res.status(200).json(mensagens);
+	// res.status(200).json("Mensagem:"+mensagens[1].mensagem);
+	res.status(200).json(mensagens);
 });
 
 app.get("/mensagens/:id", cors(corsOptions), (req, res) => {
-  const index = buscaMensagem(req.params.id);
-  res.status(200).json(mensagens[index]);
+	const index = buscaMensagem(req.params.id);
+	res.status(200).json(mensagens[index]);
 });
 
-app.post("/addmsg", cors(corsOptions), (req, res) => {
-  mensagens.push(req.body);
-  res.status(201).send("Nova msg cadastrada")
+// app.post("/addmsg", cors(corsOptions), (req, res) => {
+//   mensagens.push(req.body);
+//   res.status(201).send("Nova msg cadastrada")
+// });
+
+app.post("/addmsg", cors(corsOptions), async (req, res) => {
+	// Operador ternário=> CONDIÇÃO ? CASO SIM : CASO NÃO
+	let ultimoID =
+		mensagens.length > 0 ? mensagens[mensagens.length - 1].id : 0;
+	const novoId = ultimoID + 1;
+
+	const newMessage = {
+		id: novoId,
+		...req.body, // operador de espalhamento
+	};
+
+	mensagens.push(newMessage);
+
+	res.status(201).json({
+		message: "Nova mensagem cadastrada com sucesso!",
+		mensagem: newMessage,
+	});
 });
 
 app.put("/mensagens/:id", (req, res) => {
-  const index = buscaMensagem(req.params.id);
-  mensagens[index].nome = req.body.nome;
-  // res.status(200).json(mensagens[index]);
-  res.status(200).json(mensagens);
+	const index = buscaMensagem(req.params.id);
+	mensagens[index].nome = req.body.nome;
+	// res.status(200).json(mensagens[index]);
+	res.status(200).json(mensagens);
 });
 
 app.delete("/del/:id", cors(corsOptions), (req, res) => {
-  const index = buscaMensagem(req.params.id);
-  mensagens.splice(index,1);
-  res.status(200).json(mensagens);
+	const index = buscaMensagem(req.params.id);
+	mensagens.splice(index, 1);
+	res.status(200).json(mensagens);
 });
 
 export default app;
